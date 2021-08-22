458622fd6ffab880c1e79b6b4419f5df193a998c
Vakili <vakili@outlook.com>
2021-08-22 13:03:20 +0430

    Initialize Sizpay nopCommerce Plugin

---
+++ Controllers/SizPayController.cs
@@ -0,0 +1,255 @@
+﻿//http://nopfarsi .ir
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Nop.Core;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Payments;
+using NopFarsi.Payment.SizPay.Models;
+using Nop.Services.Common;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Orders;
+using Nop.Services.Payments;
+using Nop.Services.Security;
+using Nop.Services.Stores;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+using Nop.Services.Messages;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace NopFarsi.Payment.SizPay.Controllers
+{
+    public class SizPayController : BasePaymentController
+    {
+        #region Fields
+
+        private readonly IWorkContext _workContext;
+        private readonly IStoreService _storeService;
+        private readonly ISettingService _settingService;
+        private readonly IPaymentService _paymentService;
+        private readonly IOrderService _orderService;
+        private readonly IOrderProcessingService _orderProcessingService;
+        private readonly IPermissionService _permissionService;
+        private readonly IGenericAttributeService _genericAttributeService;
+        private readonly ILocalizationService _localizationService;
+        private readonly IStoreContext _storeContext;
+        private readonly ILogger _logger;
+        private readonly IWebHelper _webHelper;
+        private readonly PaymentSettings _paymentSettings;
+        private readonly PayPaymentSettings _payPaymentSettings;
+        private readonly INotificationService notificationService;
+        private readonly ShoppingCartSettings _shoppingCartSettings;
+
+        #endregion Fields
+
+        #region Ctor
+
+        public SizPayController(IWorkContext workContext,
+            IStoreService storeService,
+            ISettingService settingService,
+            IPaymentService paymentService,
+            IOrderService orderService,
+            IOrderProcessingService orderProcessingService,
+            IPermissionService permissionService,
+            IGenericAttributeService genericAttributeService,
+            ILocalizationService localizationService,
+            IStoreContext storeContext,
+            ILogger logger,
+            IWebHelper webHelper,
+            PaymentSettings paymentSettings,
+            PayPaymentSettings payPaymentSettings,
+            INotificationService notificationService,
+
+            ShoppingCartSettings shoppingCartSettings)
+        {
+            _workContext = workContext;
+            _storeService = storeService;
+            _settingService = settingService;
+            _paymentService = paymentService;
+            _orderService = orderService;
+            _orderProcessingService = orderProcessingService;
+            _permissionService = permissionService;
+            _genericAttributeService = genericAttributeService;
+            _localizationService = localizationService;
+            _storeContext = storeContext;
+            _logger = logger;
+            _webHelper = webHelper;
+            _paymentSettings = paymentSettings;
+            _payPaymentSettings = payPaymentSettings;
+            this.notificationService = notificationService;
+            _shoppingCartSettings = shoppingCartSettings;
+        }
+
+        #endregion Ctor
+
+        #region Methods
+
+        public IActionResult Pay(string MerchantID, string TerminalID, string Token)
+        {
+            ViewBag.MerchantID = MerchantID;
+            ViewBag.TerminalID = TerminalID;
+            ViewBag.Token = Token;
+            return base.View("~/Plugins/NopFarsi.Payment.SizPay/Views/Pay.cshtml");
+        }
+
+        [AuthorizeAdmin]
+        [Area(AreaNames.Admin)]
+        public async Task<IActionResult> Configure()
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManagePaymentMethods))
+                return AccessDeniedView();
+
+            //load settings for a chosen store scope
+            var storeScope = await this._storeContext.GetActiveStoreScopeConfigurationAsync();
+            var payPaymentSettings = await _settingService.LoadSettingAsync<PayPaymentSettings>(storeScope);
+
+            var model = new ConfigurationModel
+            {
+                MerchantID = payPaymentSettings.MerchentId,
+                UserName = payPaymentSettings.UserName,
+                Password = payPaymentSettings.Password,
+                TerminalID = payPaymentSettings.TerminalId,
+                IsToman = payPaymentSettings.IsToman
+            };
+            if (storeScope > 0)
+            {
+                model.MerchantID_OverrideForStore = await _settingService.SettingExistsAsync(payPaymentSettings, x => x.MerchentId, storeScope);
+                model.Password_OverrideForStore = await _settingService.SettingExistsAsync(payPaymentSettings, x => x.Password, storeScope);
+                model.TerminalID_OverrideForStore = await _settingService.SettingExistsAsync(payPaymentSettings, x => x.TerminalId, storeScope);
+                model.IsToman_OverrideForStore = await _settingService.SettingExistsAsync(payPaymentSettings, x => x.IsToman, storeScope);
+            }
+
+            return View("~/Plugins/NopFarsi.Payment.SizPay/Views/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        [AuthorizeAdmin]
+        //[AdminAntiForgery]
+        [Area(AreaNames.Admin)]
+        public async Task<IActionResult> Configure(ConfigurationModel model)
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManagePaymentMethods))
+                return AccessDeniedView();
+
+            if (!ModelState.IsValid)
+                return await Configure();
+
+            //load settings for a chosen store scope
+            var storeScope = await this._storeContext.GetActiveStoreScopeConfigurationAsync();
+            var payPaymentSettings = await _settingService.LoadSettingAsync<PayPaymentSettings>(storeScope);
+
+            //save settings
+            payPaymentSettings.IsToman = model.IsToman;
+            payPaymentSettings.MerchentId = model.MerchantID;
+            payPaymentSettings.UserName = model.UserName;
+            payPaymentSettings.TerminalId = model.TerminalID;
+            payPaymentSettings.Password = model.Password;
+            payPaymentSettings.IsToman = model.IsToman;
+
+            /* We do not clear cache after each setting update.
+             * This behavior can increase performance because cached settings will not be cleared
+             * and loaded from database after each update */
+            await _settingService.SaveSettingOverridablePerStoreAsync(payPaymentSettings, x => x.IsToman, model.IsToman_OverrideForStore, storeScope, false);
+            await _settingService.SaveSettingOverridablePerStoreAsync(payPaymentSettings, x => x.MerchentId, model.MerchantID_OverrideForStore, storeScope, false);
+            await _settingService.SaveSettingOverridablePerStoreAsync(payPaymentSettings, x => x.UserName, model.UserName_OverrideForStore, storeScope, false);
+            await _settingService.SaveSettingOverridablePerStoreAsync(payPaymentSettings, x => x.TerminalId, model.TerminalID_OverrideForStore, storeScope, false);
+            await _settingService.SaveSettingOverridablePerStoreAsync(payPaymentSettings, x => x.IsToman, model.IsToman_OverrideForStore, storeScope, false);
+            await _settingService.SaveSettingOverridablePerStoreAsync(payPaymentSettings, x => x.Password, model.Password_OverrideForStore, storeScope, false);
+
+            //now clear settings cache
+            await _settingService.ClearCacheAsync();
+
+            this.notificationService.SuccessNotification(await _localizationService.GetResourceAsync("Admin.Plugins.Saved"));
+
+            return await Configure();
+        }
+
+        //action displaying notification (warning) to a store owner about inaccurate Pay rounding
+        [AuthorizeAdmin]
+        [Area(AreaNames.Admin)]
+        public async Task<IActionResult> RoundingWarning(bool passProductNamesAndTotals)
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManagePaymentMethods))
+                return AccessDeniedView();
+
+            //prices and total aren't rounded, so display warning
+            if (passProductNamesAndTotals && !_shoppingCartSettings.RoundPricesDuringCalculation)
+                return Json(new { Result = await _localizationService.GetResourceAsync("Plugins.Payments.Pay.RoundingWarning") });
+
+            return Json(new { Result = string.Empty });
+        }
+
+        //[HttpPost]
+        public async Task<IActionResult> Verify(int ResCod, string Message, int MrchID, string TrmnlID, int InvoiceNo, string ExtraInf, string Token)
+        {
+            if (ResCod == 0)
+            {
+                var order = await _orderService.GetOrderByIdAsync(InvoiceNo);
+                if (order.AuthorizationTransactionCode == Token)
+                {
+                    ServicePointManager.ServerCertificateValidationCallback =
+                               delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+
+                    var kimial = new KimialPG.KimiaIPGRouteServiceSoapClient(KimialPG.KimiaIPGRouteServiceSoapClient.EndpointConfiguration.KimiaIPGRouteServiceSoap);
+                    var result = kimial.ConfirmAsync(new KimialPG.PaymentRequest()
+                    {
+                        MerchantID = _payPaymentSettings.MerchentId,
+                        Password = _payPaymentSettings.Password,
+                        UserName = _payPaymentSettings.UserName,
+                        TerminalID = _payPaymentSettings.TerminalId,
+                        Token = Token
+                    }).Result.Body.ConfirmResult;
+                    if (result.ResCod == 0)
+                    {
+                        await this._orderService.InsertOrderNoteAsync(new OrderNote
+                        {
+                            OrderId = order.Id,
+                            Note = result.TraceNo + " " + result.TransNo,
+                            DisplayToCustomer = false,
+                            CreatedOnUtc = DateTime.UtcNow,
+                        });
+                        await _orderProcessingService.MarkOrderAsPaidAsync(order);
+                        return RedirectToRoute("CheckoutCompleted", new { orderId = order.Id });
+                    }
+                    else
+                    {
+                        await this._orderService.InsertOrderNoteAsync(new OrderNote
+                        {
+                            OrderId = order.Id,
+                            Note = result.Message,
+                            DisplayToCustomer = false,
+                            CreatedOnUtc = DateTime.UtcNow,
+                        });
+                    }
+                    await _orderService.UpdateOrderAsync(order);
+                }
+            }
+            return base.RedirectToAction("Error", "SizPay", new
+            {
+                ErrorCode = ResCod,
+                Message = Message
+            });
+        }
+
+        public IActionResult Error(string ErrorCode, string Message)
+        {
+            ViewBag.ErrorCode = ErrorCode;
+            ViewBag.Message = Message;
+            return View("~/Plugins/NopFarsi.Payment.SizPay/Views/Error.cshtml");
+        }
+
+
+        #endregion Methods
+    }
+}
---
+++ PayPaymentProcessor.cs
@@ -0,0 +1,375 @@
+﻿//http://nopfarsi.ir
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using Nop.Core;
+using Nop.Core.Domain.Directory;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Shipping;
+using Nop.Core.Infrastructure;
+using Nop.Services.Common;
+using Nop.Services.Configuration;
+using Nop.Services.Directory;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Orders;
+using Nop.Services.Payments;
+using Nop.Services.Plugins;
+using Nop.Services.Tax;
+
+namespace NopFarsi.Payment.SizPay
+{
+    /// <summary>
+    /// PayStandard payment processor
+    /// </summary>
+    public class PayPaymentProcessor : BasePlugin, IPaymentMethod
+    {
+        #region Fields
+
+        private readonly CurrencySettings _currencySettings;
+        private readonly ICheckoutAttributeParser _checkoutAttributeParser;
+        private readonly ICurrencyService _currencyService;
+        private readonly IGenericAttributeService _genericAttributeService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILocalizationService _localizationService;
+        private readonly IOrderTotalCalculationService _orderTotalCalculationService;
+        private readonly ISettingService _settingService;
+        private readonly ITaxService _taxService;
+        private readonly IWebHelper _webHelper;
+        private readonly IPaymentService paymentService;
+        private readonly PayPaymentSettings _payPaymentSettings;
+        private readonly ILogger logger;
+
+        #endregion Fields
+
+        #region Ctor
+
+        public PayPaymentProcessor(CurrencySettings currencySettings,
+            ICheckoutAttributeParser checkoutAttributeParser,
+            ICurrencyService currencyService,
+            IGenericAttributeService genericAttributeService,
+            IHttpContextAccessor httpContextAccessor,
+            ILocalizationService localizationService,
+            IOrderTotalCalculationService orderTotalCalculationService,
+            ISettingService settingService,
+            ITaxService taxService,
+            IWebHelper webHelper,
+            PayPaymentSettings payPaymentSettings,
+            ILogger logger,
+            IPaymentService paymentService)
+        {
+            this._currencySettings = currencySettings;
+            this._checkoutAttributeParser = checkoutAttributeParser;
+            this._currencyService = currencyService;
+            this._genericAttributeService = genericAttributeService;
+            this._httpContextAccessor = httpContextAccessor;
+            this._localizationService = localizationService;
+            this._orderTotalCalculationService = orderTotalCalculationService;
+            this._settingService = settingService;
+            this._taxService = taxService;
+            this._webHelper = webHelper;
+            this._payPaymentSettings = payPaymentSettings;
+            this.logger = logger;
+            this.paymentService = paymentService;
+        }
+
+        #endregion Ctor
+
+        #region Utilities
+
+        /// <summary>
+        /// Gets Pay URL
+        /// </summary>
+        /// <returns></returns>
+        private string GetPayUrl()
+        {
+            return "https://sizpay.ir/payment/send";
+        }
+
+        /// <summary>
+        /// Gets IPN Pay URL
+        /// </summary>
+        /// <returns></returns>
+        private string GetIpnPayUrl()
+        {
+            return "https://sizpay.ir/payment/verify";
+        }
+
+        #endregion Utilities
+
+        #region Methods
+
+        /// <summary>
+        /// Gets a configuration page URL
+        /// </summary>
+        public override string GetConfigurationPageUrl()
+        {
+
+            return $"{_webHelper.GetStoreLocation()}Admin/SizPay/Configure";
+        }
+
+        /// <summary>
+        /// Gets a name of a view component for displaying plugin in public store ("payment info" checkout step)
+        /// </summary>
+        /// <returns>View component name</returns>
+        public string GetPublicViewComponentName()
+        {
+            return "PaymentPay";
+        }
+
+        /// <summary>
+        /// Install the plugin
+        /// </summary>
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+            await this._localizationService.AddOrUpdateLocaleResourceAsync("Plugins.Payments.Pay.Fields.UserName", "Key");
+            await this._localizationService.AddOrUpdateLocaleResourceAsync("Plugins.Payments.Pay.Fields.Password", "IV");
+            await this._localizationService.AddOrUpdateLocaleResourceAsync("Plugins.Payments.Pay.Fields.MerchantID", "Merchant Id");
+            await this._localizationService.AddOrUpdateLocaleResourceAsync("Plugins.Payments.Pay.Fields.TerminalID", "Terminal Id");
+            await this._localizationService.AddOrUpdateLocaleResourceAsync("Plugins.Payments.Pay.Fields.IsToman", "واحد پول فروشگاه تومان است");
+            await this._localizationService.AddOrUpdateLocaleResourceAsync("Plugins.Payments.SizPay.Fields.RedirectionTip", "به درگاه سیزپی منتقل می شوید.");
+        }
+
+        /// <summary>
+        /// Uninstall the plugin
+        /// </summary>
+        public override async Task UninstallAsync()
+        {
+            //settings
+            await _settingService.DeleteSettingAsync<PayPaymentSettings>();
+
+            //locales
+            await _localizationService.DeleteLocaleResourceAsync("plugins.payments.pay.Instructions");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.Api");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.Api.Hint");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.Redirect");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.Redirect.Hint");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.AdditionalFee");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.AdditionalFee.Hint");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.AdditionalFeePercentage");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.AdditionalFeePercentage.Hint");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.Fields.RedirectionTip");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.PaymentMethodDescription");
+            await _localizationService.DeleteLocaleResourceAsync("Plugins.Payments.Pay.RoundingWarning");
+
+            await base.UninstallAsync();
+        }
+
+        public Task<ProcessPaymentResult> ProcessPaymentAsync(ProcessPaymentRequest processPaymentRequest)
+        {
+            return Task.FromResult(new ProcessPaymentResult());
+        }
+
+        public async Task PostProcessPaymentAsync(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            //create common query parameters for the request
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback =
+                    delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+                var kimial = new KimialPG.KimiaIPGRouteServiceSoapClient(KimialPG.KimiaIPGRouteServiceSoapClient.EndpointConfiguration.KimiaIPGRouteServiceSoap);
+                decimal OrderTotal = postProcessPaymentRequest.Order.OrderTotal;
+                if (_payPaymentSettings.IsToman)
+                    OrderTotal = postProcessPaymentRequest.Order.OrderTotal * 10;
+                string returnUrl = this._webHelper.GetStoreLocation() + "SizPay/Verify?OrderId=" + postProcessPaymentRequest.Order.Id.ToString();
+
+                DateTime d = DateTime.Now;
+                PersianCalendar pc = new PersianCalendar();
+
+                var result1 = await kimial.GetTokenAsync(new KimialPG.GenerateToken()
+                {
+                    UserName = _payPaymentSettings.UserName,
+                    Password = _payPaymentSettings.Password,
+                    MerchantID = _payPaymentSettings.MerchentId,
+                    TerminalID = _payPaymentSettings.TerminalId,
+                    Amount = Convert.ToInt32(OrderTotal),
+                    DocDate = string.Format("{0}/{1}/{2}", pc.GetYear(d), pc.GetMonth(d), pc.GetDayOfMonth(d)),
+                    OrderID = postProcessPaymentRequest.Order.Id.ToString(),
+                    ReturnURL = returnUrl,
+                    //ExtraInf=_payPaymentSettings.InvoiceNo,
+                    InvoiceNo = postProcessPaymentRequest.Order.Id.ToString(),
+                    AppExtraInf = new KimialPG.AppExtraInf(),
+                });//.Body.GetTokenResult;
+                var result = result1.Body.GetTokenResult;
+                if (result.ResCod == 0)
+                {
+                    postProcessPaymentRequest.Order.AuthorizationTransactionCode = result.Token;
+                    await EngineContext.Current.Resolve<IOrderService>().UpdateOrderAsync(postProcessPaymentRequest.Order);
+
+                    this._httpContextAccessor.HttpContext.Response.Redirect(
+                        this._webHelper.GetStoreLocation() + "SizPay/Pay?MerchantID=" + _payPaymentSettings.MerchentId
+                        + "&TerminalID=" + _payPaymentSettings.TerminalId +
+                        "&Token=" + result.Token);
+                }
+                else
+                {
+                    await this.logger.ErrorAsync("Error Code : " + result.ResCod + " " + "Error Message : " + result.Message);
+                    this._httpContextAccessor.HttpContext.Response.Redirect(
+                        this._webHelper.GetStoreLocation() + "SizPay/Error?ErrorCode=" + result.ResCod
+                        + "&Message=" + System.Net.WebUtility.UrlEncode(result.Message));
+                }
+            }
+            catch (Exception exp)
+            {
+                await this.logger.ErrorAsync("Error Code : " + exp.Message);
+                throw new NopException("Error Message" + exp.Message);
+            }
+        }
+
+        public Task<bool> HidePaymentMethodAsync(IList<ShoppingCartItem> cart)
+        {
+            return Task.FromResult(false);
+        }
+
+        public async Task<decimal> GetAdditionalHandlingFeeAsync(IList<ShoppingCartItem> cart)
+        {
+            return await paymentService.CalculateAdditionalFeeAsync(cart, 0, false);
+        }
+
+        public Task<CapturePaymentResult> CaptureAsync(CapturePaymentRequest capturePaymentRequest)
+        {
+            CapturePaymentResult capture = new CapturePaymentResult();
+            capture.AddError("Capture method not supported.");
+            return Task.FromResult(capture);
+        }
+
+        public Task<RefundPaymentResult> RefundAsync(RefundPaymentRequest refundPaymentRequest)
+        {
+            RefundPaymentResult refund = new RefundPaymentResult();
+            refund.AddError("Refund method not supported.");
+            return Task.FromResult(refund);
+        }
+
+        public Task<VoidPaymentResult> VoidAsync(VoidPaymentRequest voidPaymentRequest)
+        {
+            VoidPaymentResult voidPay = new VoidPaymentResult();
+            voidPay.AddError("Void method not supported.");
+            return Task.FromResult(voidPay);
+        }
+
+        public Task<ProcessPaymentResult> ProcessRecurringPaymentAsync(ProcessPaymentRequest processPaymentRequest)
+        {
+            ProcessPaymentResult p = new ProcessPaymentResult();
+            p.AddError("Recurring payment not supported.");
+            return Task.FromResult(p);
+        }
+
+        public Task<CancelRecurringPaymentResult> CancelRecurringPaymentAsync(CancelRecurringPaymentRequest cancelPaymentRequest)
+        {
+            CancelRecurringPaymentResult c = new CancelRecurringPaymentResult();
+            c.AddError("Recurring payment not supported.");
+            return Task.FromResult(c);
+        }
+
+        public Task<bool> CanRePostProcessPaymentAsync(Order order)
+        {
+            if (order == null)
+            {
+                throw new ArgumentNullException("order");
+            }
+            return Task.FromResult((DateTime.UtcNow - order.CreatedOnUtc).TotalSeconds >= 5.0);
+        }
+
+        public Task<IList<string>> ValidatePaymentFormAsync(IFormCollection form)
+        {
+            if (form == null)
+            {
+                throw new ArgumentException(nameof(form));
+            }
+
+            //try to get errors
+            if (form.TryGetValue("Errors", out StringValues errorsString) && !StringValues.IsNullOrEmpty(errorsString))
+            {
+                var errorList = new List<string> { errorsString.ToString() };
+                return Task.FromResult<IList<string>>(errorList);
+            }
+
+            return Task.FromResult<IList<string>>(new List<string>());
+        }
+
+        public Task<ProcessPaymentRequest> GetPaymentInfoAsync(IFormCollection form)
+        {
+            return Task.FromResult(new ProcessPaymentRequest());
+        }
+
+        public Task<string> GetPaymentMethodDescriptionAsync()
+        {
+            return Task.FromResult(_localizationService.GetResourceAsync("Plugins.Payments.SizPay.Fields.RedirectionTip").Result);
+        }
+
+        #endregion Methods
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether capture is supported
+        /// </summary>
+        public bool SupportCapture
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether partial refund is supported
+        /// </summary>
+        public bool SupportPartiallyRefund
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether refund is supported
+        /// </summary>
+        public bool SupportRefund
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether void is supported
+        /// </summary>
+        public bool SupportVoid
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a recurring payment type of payment method
+        /// </summary>
+        public RecurringPaymentType RecurringPaymentType
+        {
+            get { return RecurringPaymentType.NotSupported; }
+        }
+
+        /// <summary>
+        /// Gets a payment method type
+        /// </summary>
+        public PaymentMethodType PaymentMethodType
+        {
+            get { return PaymentMethodType.Redirection; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether we should display a payment information page for this plugin
+        /// </summary>
+        public bool SkipPaymentInfo
+        {
+            get { return false; }
+        }
+
+
+
+        #endregion Properties
+    }
+}
